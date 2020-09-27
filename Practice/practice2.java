f4ac77c5871ab5fbb9124c3c68f2998f1f15ec69
kiriha1203 <mizumoto.takefumi@gmail.com>
2020-09-27 22:17:39 +0900

    add

---
+++ Practice/practice2.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import java.sql.Date;
+import java.time.*;
+import java.time.format.*;
+
+public class practice2 {
+  public static void main(String[] args) {
+    LocalDate now = LocalDate.now();
+    LocalDate future = now.plusDays(100);
+    DateTimeFormatter f = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
+    System.out.println(future.format(f));
+  }
+  
+}
